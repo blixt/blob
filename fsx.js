8ef4871bb7ae796a5df8718697a1aa8cd0b31b42
Blixt <me@blixt.org>
2014-04-17 12:11:44 -0400

    Update to blobs

--- fsx.js
+++ fsx.js
@@ -1,26 +1,50 @@
-var GRAVITY = 0.1;
+var GRAVITY = .1;
+var MAX_LINES = 128;
 var MAX_PARTICLES = 1024;
 
 var constraints = [];
+
+var linesX1 = new Float64Array(MAX_LINES),
+    linesY1 = new Float64Array(MAX_LINES),
+    linesX2 = new Float64Array(MAX_LINES),
+    linesY2 = new Float64Array(MAX_LINES),
+    linesNX = new Float64Array(MAX_LINES),
+    linesNY = new Float64Array(MAX_LINES),
+    linesFriction = new Float64Array(MAX_LINES);
+
 var particlesX1 = new Float64Array(MAX_PARTICLES),
     particlesY1 = new Float64Array(MAX_PARTICLES),
     particlesX2 = new Float64Array(MAX_PARTICLES),
     particlesY2 = new Float64Array(MAX_PARTICLES);
 
 var numConstraints = 0,
+    numLines = 0,
     numParticles = 0;
 
-// Create a constraint between each index specified. Make the last value `true'
-// if the constraints should be created between all particles in the set,
-// rather than in a sequence.
+// constrain(p1, p2, p3, …[, all[, hardness]])
+// number pN: particle index
+// boolean all: create constraints between ALL particles in list instead of
+//              sequence
+// number hardness: how soft/hard constraint is (0.0–1.0)
 function constrain(var_args) {
+  if (Array.isArray(var_args)) {
+    return constrain.apply(null, var_args.concat(Array.prototype.slice.call(arguments, 1)));
+  }
+
   var length = arguments.length;
 
-  var all = arguments[length - 1];
+  var all = arguments[length - 2], hardness;
   if (typeof all == 'boolean') {
-    length--;
+    hardness = arguments[length - 1];
+    length -= 2;
   } else {
-    all = false;
+    hardness = 1;
+    all = arguments[length - 1];
+    if (typeof all == 'boolean') {
+      length--;
+    } else {
+      all = false;
+    }
   }
 
   if (length < 2)
@@ -35,16 +59,54 @@ function constrain(var_args) {
 
     if (all) {
       for (var j = i + 1; j < length; j++) {
-        constrain(value, arguments[j]);
+        constrain(value, arguments[j], false, hardness);
       }
     } else {
       if (i > 0) {
-        constraints.push(previous, value, distance(previous, value));
+        constraints.push(previous, value, distance(previous, value), hardness);
         numConstraints++;
       }
       previous = value;
     }
   }
+}
+
+function createBlob(x, y, radius, opt_points, opt_vx, opt_vy) {
+  var n = opt_points || 9;
+
+  if (n < 3)
+    throw new Error('At least three points are required');
+
+  var angleStep = Math.PI * 2 / n;
+  var particles = [];
+  for (var i = 0; i < n; i++) {
+    var p = createParticle(x + Math.cos(i * angleStep) * radius,
+                           y + Math.sin(i * angleStep) * radius,
+                           opt_vx, opt_vy);
+    particles.push(p);
+  }
+
+  for (var i = 0; i < n; i++) {
+    for (var j = i + 1; j < n; j++) {
+      var neighbor = (j == i + 1 || i == 0 && j == n - 1);
+      constrain(particles[i], particles[j], false, neighbor ? 1 : .03);
+    }
+  }
+}
+
+function createLine(x1, y1, x2, y2, opt_friction) {
+  linesX1[numLines] = x1;
+  linesY1[numLines] = y1;
+  linesX2[numLines] = x2;
+  linesY2[numLines] = y2;
+
+  var dx = x2 - x1, dy = y2 - y1;
+  var length = Math.sqrt(dx * dx + dy * dy);
+  linesNX[numLines] = dy / length;
+  linesNY[numLines] = -dx / length;
+  linesFriction[numLines] = opt_friction || .2;
+
+  return numLines++;
 }
 
 function createParticle(x, y, opt_vx, opt_vy) {
@@ -65,32 +127,96 @@ function distance(a, b) {
   return Math.sqrt(dx * dx + dy * dy);
 }
 
+function line2line(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
+  var adx = ax2 - ax1, ady = ay2 - ay1,
+      bdx = bx2 - bx1, bdy = by2 - by1,
+      abx1 = ax1 - bx1, aby1 = ay1 - by1;
+
+  var d = bdy * adx - bdx * ady;
+  if (d == 0) {
+    return false;
+  }
+
+  var ua = (bdx * aby1 - bdy * abx1) / d;
+  var ub = (adx * aby1 - ady * abx1) / d;
+  if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+    return false;
+  }
+
+  return ua;
+}
+
+var MOVE_DX, MOVE_DY;
+function move(x, y, dx, dy) {
+  for (var i = 0; i < numLines; i++) {
+    var lx1 = linesX1[i], ly1 = linesY1[i],
+        lx2 = linesX2[i], ly2 = linesY2[i],
+        lnx = linesNX[i], lny = linesNY[i],
+        ldx = lx2 - lx1, ldy = ly2 - ly1;
+
+    var d = lx1 * lnx + ly1 * lny;
+
+    var startDist = x * lnx + y * lny - d;
+    if (startDist < 0) {
+      lnx = -lnx;
+      lny = -lny;
+      d = -d;
+    }
+
+    var endDist = (x + dx) * lnx + (y + dy) * lny - d;
+    if (endDist < 0) {
+      var u = (dx * (y - ly1) - dy * (x - lx1)) / (ldy * dx - ldx * dy);
+      if (u > 0 && u < 1) {
+        var friction = linesFriction[i];
+        var slideX = x + dx - (lx1 + ldx * u), slideY = y + dy - (ly1 + ldy * u);
+        dx += lnx * (-endDist + 0.001) - slideX * friction;
+        dy += lny * (-endDist + 0.001) - slideY * friction;
+      }
+    }
+  }
+
+  MOVE_DX = dx;
+  MOVE_DY = dy;
+}
+
 function step() {
   var i, e;
 
-  for (i = 0, e = numConstraints * 3; i < e; i += 3) {
+  for (i = 0, e = numConstraints * 4; i < e; i += 4) {
     var a = constraints[i],
         b = constraints[i + 1],
-        rest = constraints[i + 2];
+        rest = constraints[i + 2],
+        hardness = constraints[i + 3];
 
-    var dx = particlesX2[a] - particlesX2[b],
-        dy = particlesY2[a] - particlesY2[b],
+    var ax = particlesX2[a],
+        ay = particlesY2[a],
+        bx = particlesX2[b],
+        by = particlesY2[b],
+        dx = ax - bx,
+        dy = ay - by,
         dist = Math.sqrt(dx * dx + dy * dy),
-        diff = (rest - dist) / dist,
+        diff = (rest - dist) / dist * hardness,
         tx = dx / 2 * diff,
         ty = dy / 2 * diff;
 
-    particlesX2[a] += tx;
-    particlesY2[a] += ty;
-    particlesX2[b] -= tx;
-    particlesY2[b] -= ty;
+    move(ax, ay, tx, ty);
+    particlesX2[a] += MOVE_DX;
+    particlesY2[a] += MOVE_DY;
+    move(bx, by, -tx, -ty);
+    particlesX2[b] += MOVE_DX;
+    particlesY2[b] += MOVE_DY;
   }
 
   for (i = 0; i < numParticles; i++) {
-    particlesX1[i] = particlesX2[i] * 2 - particlesX1[i];
+    var x1 = particlesX1[i], y1 = particlesY1[i],
+        x2 = particlesX2[i], y2 = particlesY2[i];
 
-    var newY = particlesY2[i] * 2 - particlesY1[i] + GRAVITY;
-    particlesY1[i] = newY > 520 ? 520 : newY;
+    var dx = (x2 - x1) * 2,
+        dy = (y2 - y1) * 2 + GRAVITY;
+
+    move(x1, y1, dx, dy);
+    particlesX1[i] += MOVE_DX;
+    particlesY1[i] += MOVE_DY;
   }
 
   var t;
--- gfx.js
+++ gfx.js
@@ -5,31 +5,62 @@ document.body.appendChild(canvas);
 
 var gfx = canvas.getContext('2d');
 
+var mx = 0, my = 0;
+document.addEventListener('mousemove', function (event) {
+  mx = event.pageX;
+  my = event.pageY;
+});
+
 var PI2 = Math.PI * 2;
 function render() {
   gfx.clearRect(0, 0, 960, 540);
-  gfx.strokeStyle = 'rgba(0, 0, 255, .2)';
 
-  for (var i = 0, e = numConstraints * 3; i < e; i += 3) {
+  /*
+  gfx.strokeStyle = 'rgba(0, 0, 0, .3)';
+  for (var i = 0; i < numParticles; i++) {
+    var x = particlesX2[i], y = particlesY2[i];
+    gfx.beginPath();
+    gfx.moveTo(x + 3, y);
+    gfx.arc(x, y, 3, 0, PI2, true);
+    gfx.stroke();
+  }
+  */
+
+  for (var i = 0, e = numConstraints * 4; i < e; i += 4) {
     var a = constraints[i],
-        b = constraints[i + 1];
+        b = constraints[i + 1],
+        hardness = constraints[i + 3];
+
+    if (hardness < .1) continue;
 
     var x1 = particlesX2[a],
         y1 = particlesY2[a],
         x2 = particlesX2[b],
         y2 = particlesY2[b];
 
+    gfx.strokeStyle = 'rgba(0, 200, 0, ' + hardness + ')';
     gfx.beginPath();
     gfx.moveTo(x1, y1);
     gfx.lineTo(x2, y2);
     gfx.stroke();
   }
 
-  for (var i = 0; i < numParticles; i++) {
-    var x = particlesX2[i], y = particlesY2[i];
+  for (var i = 0; i < numLines; i++) {
+    var x1 = linesX1[i], y1 = linesY1[i],
+        x2 = linesX2[i], y2 = linesY2[i],
+        nx = linesNX[i], ny = linesNY[i],
+        cx = (x1 + x2) / 2, cy = (y1 + y2) / 2;
+
+    gfx.strokeStyle = '#000';
     gfx.beginPath();
-    gfx.moveTo(x + 5, y);
-    gfx.arc(x, y, 5, 0, PI2, true);
+    gfx.moveTo(x1, y1);
+    gfx.lineTo(x2, y2);
+    gfx.stroke();
+
+    gfx.strokeStyle = '#00f';
+    gfx.beginPath();
+    gfx.moveTo(cx, cy);
+    gfx.lineTo(cx + nx * 10, cy + ny * 10);
     gfx.stroke();
   }
 }
